d4f54bc944bf3b09a3215d1bb7c17014def9c217
Dorah <isdoramanga9@gmail.com>
2024-02-03 15:46:52 +0300

    adding database

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Database configuration
 const pool = new Pool({
     user: 'postgres',
-    host: 'dorah',
+    host: 'localhost',
     database: 'software',
     password: 'manga13',
     port: 5432, // default Port
@@ -37,15 +37,7 @@ app.get('/register', (req, res) => {
 app.get('/dashboard', async (req, res) => {
     try {
         const client = await pool.connect();
-        const result = await client.query('SELECT
-    students.student_name,
-    supervisors.supervisor_name,
-    supervisors.supervisor_email,
-    students.allocation_date
-FROM
-    students
-JOIN
-    supervisors ON students.supervisor_id = supervisors.supervisor_id;');
+        const result = await client.query('SELECT students.student_name,supervisors.supervisor_name,supervisors.supervisor_email,students.allocation_date FROM students INNERJOIN supervisors ON students.supervisor_id = supervisors.supervisor_id;');
         const rows = result.rows;
         client.release();
         res.render('dashboard', { rows });
